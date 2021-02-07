1153c29948d4d568916583f77a6aa6f2550342bf
TrevorWaters <atrevorwaters@gmail.com>
2021-02-07 15:25:27 -0500

    Added Kristin's site

---
+++ kristinsite/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ["./Client/Pages/**/*.razor", "./Client/**/*.html", "./Shared/**/*.razor"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
