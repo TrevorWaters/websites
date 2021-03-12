6e4778542d09245745713ae8a7b19d0f7ef3ed18
Trevor Waters <atrevorwaters@gmail.com>
2021-03-11 19:41:58 -0500

    Adding business site

---
+++ fostereddesigns/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    content: ["./Pages/**/*.razor", "./**/*.html", "./Shared/**/*.razor"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'hero-pattern': "url('\assets\images\guyisinrafters.jpeg')"
+       })
+    },
+  },
+  variants: {
+    extend: {
+    }
+  },
+  plugins: [],
+}
