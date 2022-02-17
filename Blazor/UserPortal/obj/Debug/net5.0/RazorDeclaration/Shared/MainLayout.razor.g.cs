7fbfa92029dfd58b3990172f7529d4d6c3370057
Andrew, Trevor <Trevor.Andrew@flhealth.gov>
2022-02-16 23:33:51 -0500

    restructuring

--- Blazor/UserPortal/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ Blazor/UserPortal/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -4,180 +4,165 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace UserPortal.Shared.Components
+namespace UserPortal.Shared
 {
     #line hidden
     using System;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 1 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 2 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 3 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 4 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 5 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 6 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 7 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 8 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 9 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using Microsoft.AspNetCore;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 10 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using UserPortal;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 11 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using UserPortal.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 12 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using UserPortal.Shared.Components;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 13 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using UserPortal.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 14 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using UserPortal.ViewModels;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 15 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Extensions.Head;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 16 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 16 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using System.Linq;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 17 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 17 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using Microsoft.EntityFrameworkCore;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 18 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 18 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using System.Collections.Generic;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 19 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 19 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using System.Threading.Tasks;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 20 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 20 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using System.ComponentModel.DataAnnotations;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 21 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\_Imports.razor"
+#line 21 "C:\Users\atrev\source\repos\Personal\old\UserPortal\_Imports.razor"
 using UserPortal.Extensions;
 
 #line default
 #line hidden
 #nullable disable
-    public partial class ModalWindow : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class MainLayout : LayoutComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 66 "C:\Users\taw11\Desktop\Hobbies\Code\Personalwebsite\UserPortal\Shared\Components\ModalWindow.razor"
-       
-    [Parameter]
-    public string Message { get; set; }
-    [Parameter]
-    public bool Hide { get; set; } = true;
-    [Parameter]
-    public EventCallback<bool> OnClickCallback { get; set; }
-
-
-
-#line default
-#line hidden
-#nullable disable
     }
 }
 #pragma warning restore 1591
